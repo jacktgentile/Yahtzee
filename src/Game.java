be01a757d8d4a5ea4e23fb1903b2c418853c09de
vhu3 <vhu3@DESKTOP-CANCK8P>
2017-12-07 15:53:15 -0600

    12/7/2017 1553

--- src/Game.java
+++ src/Game.java
@@ -1,4 +1,18 @@
+//import java.util.Scanner;
+
+/**
+ * @author Vincent Hu, Jack Gentile
+ */
+
+/**
+ * A class that runs the game.
+ */
 
 public class Game {
-
+	public static void main(String[] args) {
+		// Welcome and where to read rules.
+		System.out.println("---------------------");
+		System.out.println(" Welcome to Yahtzee!\n Rules : https://www.hasbro.com/common/instruct/Yahtzee.pdf");
+		System.out.println("---------------------");
+	}
 }
--- src/Yahtzee.java
+++ src/Yahtzee.java
@@ -1,16 +1,11 @@
-import java.util.Scanner;
+/**
+ * @author Vincent Hu, Jack Gentile
+ *
+ */
 
+/**
+ * Class to manage the rules and functions of the game.
+ */
 public class Yahtzee {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		// title and game information
-		System.out.println("---------------------");
-		System.out.println(" Welcome to Yahtzee!\n For rules, go to https://www.hasbro.com/common/instruct/Yahtzee.pdf");
-		System.out.println("---------------------");
-		System.out.println(" To start, enter a name for each player:\n");
-		
-	}
 
 }
