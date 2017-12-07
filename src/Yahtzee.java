d01fa5973e6bc83bf4b2b071a3b4972534c723fb
RazorX <RazorX@DESKTOP-CANCK8P>
2017-12-07 14:37:56 -0600

    Changed the organiztion of the class

--- src/Yahtzee.java
+++ src/Yahtzee.java
@@ -11,22 +11,6 @@ public class Yahtzee {
 		System.out.println("---------------------");
 		System.out.println(" To start, enter a name for each player:\n");
 		
-		// prompt user for information to construct players 1 and 2
-		String name1 = "", name2 = "";
-		Scanner scan = new Scanner(System.in);
-		while (name1.equals("")) {
-			System.out.print("Player 1: ");
-			name1 = scan.next();
-		}
-		Player p1 = new Player(name1);
-		while (name2.equals("")) {
-			System.out.print("Player 2: ");
-			name2 = scan.next();
-		}
-		Player p2 = new Player(name2);
-		Game ggNoRe = new Game(p1, p2);
-		ggNoRe.start();
-		
 	}
 
 }
